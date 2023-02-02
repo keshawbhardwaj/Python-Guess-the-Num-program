e2910ad8cc6437ee73f69077de9a9b3f4dbf96b3
Keshaw Bhardwaj <119773619+keshawbhardwaj@users.noreply.github.com>
2023-02-02 22:53:30 +0530

    Add files via upload

---
+++ Guess the num Excercise.py
@@ -0,0 +1,20 @@
+n = 18
+#program for guessing number
+
+print("Guess The Number between 1 - 50")
+print("Only 5 chances are available")
+Chance_available = int()
+while (Chance_available <= 5):
+    Number = int(input("Enter the Guess: "))
+    if Number < n:
+        print("Guess a higher number")
+        print("Number of Guess left:",5 - Chance_available)
+    elif Number > n:
+        print("Guess a lower number")
+    else:
+        print("Absolutely Correct Guess")
+        break
+        if Chance_available == 5:
+            break
+            print("No more guesses are left")
+    Chance_available = Chance_available + 1
